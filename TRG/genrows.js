5b8eb44b8662f047a02ad9d6902af61c977eb892
Marc Schouwenberg <30976459+MarcSchouwenberg@users.noreply.github.com>
2020-10-17 18:11:54 +0200

    TRG flexbox+ refactor

--- TRG/genrows.js
+++ TRG/genrows.js
@@ -1,24 +1,18 @@
 var notes = ["C", "G", "D", "A", "E", "B", "F&#9839", "C&#9839", "G&#9839", "D&#9839", "A&#9839", "F"]
-// var notes = ["C ", "G ", "D ", "A ", "E ", "B ", "F#", "C#", "G#", "D#", "A#", "F "]
-var child;
-var parent;
+var parent, newRow, newNote, nums = [], inv = [];
 
 function generate(){
-	// GENERATE TONEROWS
-	//NUMS MAKEN (1,2,3,4,5,6,7,8,9,10,11,12)
-	var nums = [];
-	for (i = 0; i < 12; i++){
-		nums[i] = i+1;
+	// generate tone rows
+	// nums (1,2,3,4,5,6,7,8,9,10,11,12)
+	for (i = 1; i < 13; i++){
+		nums[i] = i;
 	}
-	   
-	//RANDOMIZE
+
+	// randomize
 	nums.sort(function(a, b){return 0.5 - Math.random()});
 
-	//INVERTED MAKEN
-	var inv = [];
-	
+	// inversion
 	inv[0] = nums[0];
-
 	for (i=1;i<12;i++){
 		var n = nums[0]*2-nums[i];
 		while(n>12){n-=12;}
@@ -26,51 +20,27 @@ function generate(){
 		inv[i]=n;
 	}
 
-	// console.log("indexes van nums: "+nums+"\n");
-
-	child = document.createElement("span");
 	parent = document.getElementById("pr");
+	newRow = document.createElement("span");
 	for (j = 0; j<12; j++){
-		var newNote = document.createElement("span");
+		newNote = document.createElement("span");
 		newNote.innerHTML = notes[nums[j]-1];
-		child.appendChild(newNote);
+		newRow.appendChild(newNote);
 	}
-	parent.replaceChild(child, parent.children[1]);
+	parent.replaceChild(newRow, parent.children[1]);
 
-	// nums.sort();
-	nums.reverse();
-	// console.log("indexes van rev: "+nums+"\n");
-
-	child = document.createElement("span");
 	parent = document.getElementById("re");
-	for (j = 0; j<12; j++){
-		var newNote = document.createElement("span");
-		newNote.innerHTML = notes[nums[j]-1];
-		child.appendChild(newNote);
-	}
-	parent.replaceChild(child, parent.children[1]);
+	parent.replaceChild(newRow.cloneNode(true), parent.children[1]);
 
-	// console.log("indexes van inv: "+inv+"\n");
-	   
-	child = document.createElement("span");
 	parent = document.getElementById("iv");
+	newRow = document.createElement("span");
 	for (j = 0; j<12; j++){
-		var newNote = document.createElement("span");
+		newNote = document.createElement("span");
 		newNote.innerHTML = notes[inv[j]-1];
-		child.appendChild(newNote);
+		newRow.appendChild(newNote);
 	}
-	parent.replaceChild(child, parent.children[1]);
+	parent.replaceChild(newRow, parent.children[1]);
 
-	// inv.sort();
-	inv.reverse();
-	// console.log("indexes van inv: "+inv+"\n");
-
-	child = document.createElement("span");
 	parent = document.getElementById("ir");
-	for (j = 0; j<12; j++){
-		var newNote = document.createElement("span");
-		newNote.innerHTML = notes[inv[j]-1];
-		child.appendChild(newNote);
-	}
-	parent.replaceChild(child, parent.children[1]);
+	parent.replaceChild(newRow.cloneNode(true), parent.children[1]);
 }
