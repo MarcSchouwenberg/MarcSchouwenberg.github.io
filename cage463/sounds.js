df6cf9fce57e253871d77779dfaad420d7a72874
MarcSchouwenberg <30976459+MarcSchouwenberg@users.noreply.github.com>
2024-06-23 11:55:02 +0200

    update sounds

--- cage463/sounds.js
+++ cage463/sounds.js
@@ -1,47 +1,47 @@
 var sounds1 = [
 "Joost",
-"#1",
-"#2",
-"#3",
-"#4",
-"#5",
-"#6",
-"#7",
-"#8",
-"#9",
-"#10",
-"#11",
-"#12",
+"Es", // Es
+"G", // G
+"B", // B
+"d - ebow", // d
+"Hals", // fis
+"a# - ebow", // bes
+"f'", // f'
+"a' - steen", // a'
+"Cis - slide", // cis''
+"e''", // e''
+"G#", // as''
+"c''' - steen", // c'''
 ];
 var sounds2 = [
 "Marc",
 "E2 - gitaar, laagste noot",
-"A#2 - synth",
+"A♭2 - synth",
 "C3 - gitaar",
-"Eb3 - synth",
+"E♭3 - synth",
 "G3 - gitaar",
 "B3 - synth",
 "D4 - glsp, laagste D", // ipv gitaar
-"F#4 - glsp, laagste F#", // ipv synth
-"Bb4 - tw1, laagste noot",
+"F♯4 - glsp, laagste F♯", // ipv synth
+"B♭4 - tw1, laagste noot",
 "F5 - tw1, 5e noot",
 "A5 - tw2, 5e noot",
-"C#5 - tw2, 7e noot",
+"C♯5 - tw2, 7e noot",
 ];
 var sounds3 = [
 "Floris",
-"#1",
-"#2",
-"#3",
-"#4",
-"#5",
-"#6",
-"#7",
-"#8",
-"#9",
-"#10",
-"#11",
-"#12",
+"#1: F", // F
+"#2: A", // A
+"#3: cis", // cis
+"#4: e", // e
+"#5: as", // as
+"#6: c'", // c'
+"#7: es'", // es'
+"#8: g'", // g'
+"#9: b'", // b'
+"#10: d''", // d''
+"#11: fis''", // fis''
+"#12: bes''", // bes''
 ];
 var sounds4 = [
 "Steven",
